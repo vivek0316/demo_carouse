ef67665d471458a66862c05c01332565e4f131e2
vivek0316 <68239109+vivek0316@users.noreply.github.com>
2025-07-25 20:03:27 +0530

    Add files via upload

--- demo_carousel/src/App.jsx
+++ demo_carousel/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useSyncExternalStore } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const imagess = [
@@ -6,47 +6,45 @@ const imagess = [
   "https://picsum.photos/id/1016/800/400",
   "https://picsum.photos/id/1018/800/400",
 ];
-function App() {
-  const [swapImg, setSwapImg] = useState("");
 
-  const handleChangeImg = (index, name) => {
-    //change img
+function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleChangeImg = (direction) => {
+    if (direction === "next") {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === imagess.length - 1 ? 0 : prevIndex + 1
+      );
+    } else if (direction === "prev") {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? imagess.length - 1 : prevIndex - 1
+      );
+    }
   };
+
   return (
-    <div className="flex justify-center items-center">
-      {imagess?.length > 0
-        ? imagess?.map((items, index) => (
-            <div key={index} className={`flex flex-row w-fit items-center `}>
-              {/* {index > 0 ? (
-                ""
-              ) : (
-                <> */}
-              <button
-                className={`w-16 h-10 bg-blue-500 rounded-sm mx-4 ${
-                  index > 0 ? "hidden" : ""
-                }`}
-                onClick={() => handleChangeImg(index, "prev")}
-              >
-                Prev
-              </button>
-              <img
-                src={items}
-                alt={items}
-                className={`w-full ${index > 0 ? "hidden" : ""}`}
-              />
-              <button
-                className={`w-16 h-10 bg-blue-500 rounded-sm mx-4 ${
-                  index > 0 ? "hidden" : ""
-                }`}
-                onClick={() => handleChangeImg(index, "next")}
-              >
-                Next
-              </button>
-              {/* </>
-              )} */}
-            </div>
-          ))
-        : "No data"}
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-row w-fit items-center">
+        <button
+          className="w-16 h-10 bg-blue-500 text-white rounded-sm mx-4"
+          onClick={() => handleChangeImg("prev")}
+        >
+          Prev
+        </button>
+
+        <img
+          src={imagess[currentIndex]}
+          alt={`Image ${currentIndex}`}
+          className="w-[800px] h-[400px] object-cover rounded"
+        />
+
+        <button
+          className="w-16 h-10 bg-blue-500 text-white rounded-sm mx-4"
+          onClick={() => handleChangeImg("next")}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
